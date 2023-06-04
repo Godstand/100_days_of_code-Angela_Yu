8ac448c64defea2ab70e0b419849b4b47b8ab27f
godstand <godstand1@gmail.com>
2023-06-04 13:16:06 +0100

    Day 26 - Listing comprehension

--- 100 days of code - Angela Yu/Day 26 - Intermediate - List Comprehension and the NATO Alphabet/nato.py
+++ 100 days of code - Angela Yu/Day 26 - Intermediate - List Comprehension and the NATO Alphabet/nato.py
@@ -24,7 +24,7 @@ data = pandas.read_csv("nato_phonetic_alphabet.csv")
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 print(phonetic_dict)
 
-#TODO 2. Create a list of phonetic code words from a word that the user inputs
+# TODO 2. Create a list of phonetic code words from a word that the user inputs
 user_input = input("Enter the word: ").upper()
 phonetic_code = [phonetic_dict[letter] for letter in user_input]
 print(phonetic_code)
